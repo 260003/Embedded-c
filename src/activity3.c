9e1f51a36e036a915d6a3bf6343dcf7844e3cab6
260003 <80737226+260003@users.noreply.github.com>
2021-05-01 18:16:52 +0530

    Update activity3.c

--- src/activity3.c
+++ src/activity3.c
@@ -1,9 +1,6 @@
 #include "activity1.h"
 #include "activity3.h"
 
-#define DutyCycle(Percent) (1024*Percent/100)
-
-
 
 void InitPWM(void){
     TCCR1A=(1<<COM1A1)|(1<<WGM11)|(1<<WGM10);
@@ -14,22 +11,22 @@ void InitPWM(void){
 char CreatePWM(uint16_t Temperature){
     InitPWM();/* Initialize Peripherals for PWM */
     if(Temperature<=200){
-         OCR1A=DutyCycle(20.0);
+         OCR1A=204.8;//PWM of Duty cycle 20% :- (20*1024/100)=204.8
         Delay(200);
         return 'w';
     }
     else if(Temperature>200 && Temperature<=500){
-         OCR1A=DutyCycle(40.0);//PWM of Duty cycle 40%
+        OCR1A=409.6;//PWM of Duty cycle 40% :- (40*1024/100)=409.6
         Delay(200);
         return 'x';
     }
     else if(Temperature>500 && Temperature<=700){
-         OCR1A=DutyCycle(70.0);//PWM of Duty cycle 70%
+         OCR1A=716.8;//PWM of Duty cycle 70% :-  (70*1024/100)=716.8
         Delay(200);
         return 'y';
     }
     else{
-         OCR1A=DutyCycle(95.0);//PWM of Duty cycle 95%
+         OCR1A=972.8;//PWM of Duty cycle 95%  :- (95*1024/100)=972.8
         Delay(200);
         return 'z';
     }
